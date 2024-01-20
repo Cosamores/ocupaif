8e6cb7c4a4c705d0049e07c828e5db7d79046b6e
Diego Soares <diegocosamores@gmail.com>
2024-01-20 15:53:54 -0300

    revolução: versão após sugestões de design da Yasmin. Agora sim isso parece um site kk - editado o estilo dos eventos quando abertos 2

--- frontend/src/components/DetalhesEvento.jsx
+++ frontend/src/components/DetalhesEvento.jsx
@@ -7,18 +7,23 @@ const DetalhesEvento = ({ imagem, nome, descricao, data, local, eventoId }) => {
   console.log(`EventoId no DetalhesEvento: ${eventoId}`)
   return (
     <div className={styles.detalhesEvento}>
-      <Image
-        className={styles.imagem}
-        src={imagem}
-        alt={nome}
-        width={500}
-        height={300}
-      />
-      <h2 className={styles.nome}>{nome}</h2>
-      <p className={styles.descricao}>{descricao}</p>
-      <p className={styles.data}>{data} </p>
-      <p className={styles.local}>Local: {local}</p>
-      <Comentarios eventoId={eventoId} />
+      <div className={styles.eventHeader}>
+        <Image
+          className={styles.imagem}
+          src={imagem}
+          alt={nome}
+          width={300}
+          height={300}
+        />
+      </div>
+      <div className={styles.eventBody}>
+        <h2 className={styles.nome}>{nome}</h2>
+        <p className={styles.descricao}>{descricao}</p>
+        <p className={styles.data}>{data} </p>
+        <p className={styles.local}>Local: {local}</p>
+      
+        <Comentarios eventoId={eventoId} className={styles.comentarios} />
+      </div>
     </div>
   );
 };
